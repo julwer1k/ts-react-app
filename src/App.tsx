c2235771a120c4b1aa8fab75703aa0000ee99f64
ArtyWhit <julwer1k@gmail.com>
2024-03-21 18:37:39 +0200

    reducer task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import { Footer } from './components/Footer';
-import { HomePage } from './components/HomePage';
-import { LangProvider } from './components/LangContext';
-import { LangSelector } from './components/LangSelector';
+import { useReducer, useState } from 'react';
+
+interface Action {
+	type: string;
+	payload?: number;
+}
+
+function reducer(amount: number, action: Action) {
+	switch (action.type) {
+		case 'decrease':
+			return amount - 1;
+		case 'increase':
+			return amount + 1;
+
+		case 'add':
+			return amount + (action.payload || 0);
+		default:
+			return amount;
+	}
+}
 
 export const App = () => {
+	const [amount, dispatch] = useReducer(reducer, 3);
+
+	const decrease = () => {
+		// setAmount(x => x - 1);
+		dispatch({ type: 'decrease' });
+	};
+
+	const increase = () => {
+		// setAmount(x => x + 1);
+		dispatch({ type: 'increase' });
+		dispatch({ type: 'add', payload: 10 });
+	};
 
 	return (
-		<div className="App">
-			<LangProvider>
-				<header className="header">
-					Mate academy
-
-					<LangSelector />
-				</header>
-
-				<main>
-					<HomePage />
-				</main>
-
-				<Footer />
-			</LangProvider>
-		</div>
+		<main>
+			<button onClick={decrease}>-</button>
+			{amount}
+			<button onClick={increase}>+</button>
+		</main>
 	);
 };
